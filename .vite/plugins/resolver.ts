7260e07433b6ac66fb3fc3e9be2b107fb1f2bcc6
Jean Barriere <jean@barriere.io>
2020-12-08 16:32:10 +0100

    chore(build): clean resolver (based on resolve-ts-aliases) + add CNAME into public

--- .vite/plugins/resolver.ts
+++ .vite/plugins/resolver.ts
@@ -5,9 +5,9 @@ export function resolveTsAliases (tsconfigPath): Record<string, string> {
   const pathPrefix = path.resolve(path.dirname(tsconfigPath), baseUrl)
   const aliases = {}
   Object.keys(paths).forEach((item) => {
-    const name = item.replace(/^(.*)\/\*$/, '$1/')
+    const name = item.replace('/*', '/')
     const value = path.resolve(pathPrefix, paths[item][0].replace('/*', ''))
-    aliases[name] = `${value}`
+    aliases[name] = value
   })
   return aliases
 }
